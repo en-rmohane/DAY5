4d91780365c65038c0aea4149a079c477b2466d3
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:19:06 +0530

    [ravi kumar]:-largest number

---
+++ laegestnumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        int largestNumber = firstNumber;
+
+        if (secondNumber > largestNumber)
+        {
+            largestNumber = secondNumber;
+        }
+
+        if (thirdNumber > largestNumber)
+        {
+            largestNumber = thirdNumber;
+        }
+
+        Console.WriteLine("The largest number is: {0}", largestNumber);
+    }
+}
