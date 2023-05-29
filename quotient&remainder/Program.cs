a92c49dc0d0cdf8ccacf4a19688ec1c5f7d0db8b
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:25:31 +0530

    [ravi kumar]:- quotient&remainder

---
+++ quotient&remainder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int dividend = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter another number (divisor): ");
+        int divisor = int.Parse(Console.ReadLine());
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        Console.WriteLine("Quotient: {0}", quotient);
+        Console.WriteLine("Remainder: {0}", remainder);
+    }
+}
