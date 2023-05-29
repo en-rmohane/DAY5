a60d0b89cd9f2c44c9b03d4da1a250e4a5e0cf30
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:22:10 +0530

    [ravi kumar]:-power of 2

---
+++ powerof2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Powers of 2 table for {0}:", userInput);
+
+        for (int i = 0; i <= userInput; i++)
+        {
+            Console.WriteLine("2^{0} = {1}", i, Math.Pow(2, i));
+        }
+    }
+}
