02e0bcdb52e60160a781debfa65041f9a6af83b3
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:04:14 +0530

    [ravi kumar]:-program1

---
+++ alphabet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an alphabet: ");
+        char alphabet = Console.ReadLine().ToLower()[0];
+
+        if (char.IsLetter(alphabet))
+        {
+            if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u')
+            {
+                Console.WriteLine("The entered alphabet is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("The entered alphabet is a consonant.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid alphabet.");
+        }
+    }
+}
