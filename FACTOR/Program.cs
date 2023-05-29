fc2d26415fe017cf83b20540aaa1535f305e08cc
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:10:31 +0530

    [RAVI KUMAR]:- program2

---
+++ FACTOR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Factors of {0} are:", number);
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
