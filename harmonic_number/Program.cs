118c342094be75c8c1b00c3201efa9fee1ab11ac
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:16:18 +0530

    [ravi kumar]:-program 4

---
+++ harmonic_number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (N): ");
+        int n = int.Parse(Console.ReadLine());
+
+        double harmonicNumber = 0.0;
+        for (int i = 1; i <= n; i++)
+        {
+            harmonicNumber += 1.0 / i;
+        }
+
+        Console.WriteLine("The {0}th harmonic number is: {1}", n, harmonicNumber);
+    }
+}
