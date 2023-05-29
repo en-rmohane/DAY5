1c1060b3c9e5b25d19c8e9e7842796ee95c84210
“ravi <ravikumarmohane@gmail.com>
2023-05-29 18:28:05 +0530

    [ravi kumar]:-swaping program

---
+++ swaping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Before swapping:");
+        Console.WriteLine("First number: {0}", firstNumber);
+        Console.WriteLine("Second number: {0}", secondNumber);
+
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+
+        Console.WriteLine("After swapping:");
+        Console.WriteLine("First number: {0}", firstNumber);
+        Console.WriteLine("Second number: {0}", secondNumber);
+    }
+}
